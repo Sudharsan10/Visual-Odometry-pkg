d3aa0cbc675256ef4917af23b0824fc0f2a75d46
Sudharsan <sudharsansci@gmail.com>
2020-08-26 21:43:31 -0400

    base content algorithms.py, data_preprocessor.py, handler.py
    Signed-off-by: Sudharsan <sudharsansci@gmail.com>

---
+++ visual_odometry_pkg/algorithms.py
@@ -0,0 +1,4 @@
+
+if __name__ == '__main__':
+    msg = 'Algorithm Module of Visual odometry package.'
+    print(f'{msg}')
---
+++ visual_odometry_pkg/data_preprocessor.py
@@ -0,0 +1,4 @@
+
+if __name__ == '__main__':
+    msg = 'data preprocessor Module of Visual odometry package.'
+    print(f'{msg}')
