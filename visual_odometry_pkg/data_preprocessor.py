2a1a9ade7b07646d81d11bf77cf6eb68b60463c0
Sudharsan <sudharsansci@gmail.com>
2020-08-28 22:01:02 -0400

    Add skeleton structure files
    Files:
        - data_preprocessor.py
        - visualizer.py
    Signed-off-by: Sudharsan <sudharsansci@gmail.com>

--- visual_odometry_pkg/data_preprocessor.py
+++ visual_odometry_pkg/data_preprocessor.py
@@ -1,3 +1,32 @@
+import numpy as np
+
+
+class DataPreprocessor:
+    """
+
+
+
+    """
+
+    def __init__(self):
+        """
+
+        """
+        pass
+
+    def frames_to_video(self, source: str, destination: str) -> None:
+        """
+
+        Args:
+            source: string
+                source folder path
+            destination: string
+                desired destination folder path
+
+        Returns: None
+        """
+        pass
+
 
 if __name__ == '__main__':
     msg = 'data preprocessor Module of Visual odometry package.'
