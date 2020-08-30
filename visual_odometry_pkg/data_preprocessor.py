737dbc97fd3e2d50dc02f4e34d6c34044b72a611
Sudharsan <sudharsansci@gmail.com>
2020-08-30 19:45:47 -0400

    Feat: add frames_to_video()
    Feat:
        - supports .mjpg, .divx, .xvid
    Signed-off-by: Sudharsan <sudharsansci@gmail.com>

--- visual_odometry_pkg/data_preprocessor.py
+++ visual_odometry_pkg/data_preprocessor.py
@@ -1,10 +1,11 @@
-import numpy as np
+import glob
+import cv2 as cv
 
 
 class DataPreprocessor:
     """
 
-
+    This class object will provide set of methods for utility functions such as frames to video conversion
 
     """
 
@@ -14,10 +15,16 @@ class DataPreprocessor:
         """
         pass
 
-    def frames_to_video(self, source: str, destination: str) -> None:
+    @staticmethod
+    def frames_to_video(source: str, destination: str, file_format: str, fps: int = 24) -> None:
         """
+        If a data is presented in frames it can be converted into a video file.
 
         Args:
+            fps: int
+                number of frames per second for output video
+            file_format: string
+                file format specifier (supports )
             source: string
                 source folder path
             destination: string
@@ -25,7 +32,29 @@ class DataPreprocessor:
 
         Returns: None
         """
-        pass
+
+        files = glob.glob(source+'*.PNG')
+
+        frames_array = []
+        for x in files:
+            keyframes = cv.imread(x)
+            frames_array.append(keyframes)
+
+        height, width, layers = frames_array[0].shape
+        size = (height, width)
+        if file_format == 'DIVX':
+            video_out = cv.VideoWriter(destination, cv.VideoWriter_fourcc(*'DIVX'), fps, size)
+        elif file_format == 'XVID':
+            video_out = cv.VideoWriter(destination, cv.VideoWriter_fourcc(*'XVID'), fps, size)
+        elif file_format == 'MJPG':
+            video_out = cv.VideoWriter(destination, cv.VideoWriter_fourcc(*'MJPG'), fps, size)
+        else:
+            print("Video Format provided not supported.")
+            return
+
+        for keyframe in frames_array:
+            video_out.write(keyframe)
+        video_out.release()
 
 
 if __name__ == '__main__':
