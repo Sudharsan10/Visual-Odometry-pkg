040f2aacb74ba61d16fab83b943d8ab341d85928
Sudharsan <sudharsansci@gmail.com>
2020-09-02 06:41:01 -0400

    Feat: data_preprocesor.py - features update
    Feat:
        - color conversion
        - undistort image
    Signed-off-by: Sudharsan <sudharsansci@gmail.com>

--- visual_odometry_pkg/data_preprocessor.py
+++ visual_odometry_pkg/data_preprocessor.py
@@ -1,5 +1,7 @@
 import glob
 import cv2 as cv
+import numpy as np
+from scipy.ndimage import map_coordinates as interp2
 
 
 class DataPreprocessor:
@@ -10,9 +12,6 @@ class DataPreprocessor:
     """
 
     def __init__(self):
-        """
-
-        """
         pass
 
     @staticmethod
@@ -33,7 +32,7 @@ class DataPreprocessor:
         Returns: None
         """
 
-        files = glob.glob(source+'*.PNG')
+        files = glob.glob(source + '*.PNG')
 
         frames_array = []
         for x in files:
@@ -55,6 +54,77 @@ class DataPreprocessor:
         for keyframe in frames_array:
             video_out.write(keyframe)
         video_out.release()
+
+    @staticmethod
+    def undistort_image(image: np.array, lut: np.array) -> np.array:
+        """
+
+        Takes an distorted image and undistort it.
+
+        Args:
+            lut: np.array
+                Undistortion lookup table
+            image: np.array
+                input image
+
+        Returns: np.array
+
+        """
+        reshaped_lut = lut[:, 1::-1].T.reshape((2, image.shape[0], image.shape[1]))
+        undistorted = np.rollaxis(np.array([interp2(image[:, :, channel], reshaped_lut, order=1)
+                                            for channel in range(0, image.shape[2])]), 0, 3)
+        return undistorted.astype(image.dtype)
+
+    @staticmethod
+    def convert_bayer_bg2bgr(bayer_image: np.array) -> np.array:
+        """
+        Convert Bayer BG image to bgr
+        Args:
+            bayer_image: np.array
+                input bayer image
+        Returns:
+            BGR Image
+        """
+        return cv.cvtColor(bayer_image, cv.COLOR_BAYER_BG2BGR)
+
+    @staticmethod
+    def convert_bayer_gb2bgr(bayer_image: np.array) -> np.array:
+        """
+        Convert Bayer GB image to bgr
+        Args:
+            bayer_image: np.array
+                input bayer array
+
+        Returns:
+            BGR Image
+        """
+        return cv.cvtColor(bayer_image, cv.COLOR_BAYER_GB2BGR)
+
+    @staticmethod
+    def convert_bayer_rg2bgr(bayer_image: np.array) -> np.array:
+        """
+        Convert Bayer RG image to bgr
+        Args:
+            bayer_image: np.array
+                input bayer array
+
+        Returns:
+            BGR Image
+        """
+        return cv.cvtColor(bayer_image, cv.COLOR_BAYER_RG2BGR)
+
+    @staticmethod
+    def convert_bayer_gr2bgr(bayer_image: np.array) -> np.array:
+        """
+        Convert Bayer GR image to bgr
+        Args:
+            bayer_image: np.array
+                input bayer array
+
+        Returns:
+            BGR Image
+        """
+        return cv.cvtColor(bayer_image, cv.COLOR_BAYER_GR2BGR)
 
 
 if __name__ == '__main__':
